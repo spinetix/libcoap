b66dc5c941bc4d14a42b129d337767b59cdf48c1
Diego Santa Cruz <Diego.SantaCruz@spinetix.com>
2017-05-22 16:50:53 +0200

    revert change that references autoconf268 / autoreconf268 directly
    Directly referencing a specific name of autoconf / autoreconf files from
    autogen.sh is not a good idea, as they depend on the build system, so revert
    the change and reference vanilla autoconf / autoreconf.

--- autogen.sh
+++ autogen.sh
@@ -59,7 +59,7 @@ else
 fi
 
 # checking for autoreconf
-check_helper autoconf268
+check_helper autoconf
 if [ "$RET" = "1" ]; then
     echo "You probably need to install the package 'autoconf'."
     ERROR=1
@@ -125,7 +125,7 @@ test -n "$srcdir" || srcdir=.
 mkdir -p $srcdir/m4
 
 echo "Generating needed autotools files for $PROJECT by running autoreconf ..."
-autoreconf268 --force --install --verbose "$srcdir"
+autoreconf --force --install --verbose "$srcdir"
 
 echo
 echo "You can now run 'configure --help' to see possible configuration options."
